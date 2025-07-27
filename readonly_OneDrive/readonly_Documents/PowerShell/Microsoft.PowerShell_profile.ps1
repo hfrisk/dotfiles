5c54c65add3f307551995e062d25c195fedda8f9
Hunter Frisk <hfrisk@protonmail.com>
2025-07-27 09:38:38 -0500

    ia

---
+++ readonly_OneDrive/readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,20 @@
+# ALIASES
+#-------------------------
+Set-Alias -Name ls -Value eza -Option AllScope
+Set-Alias -Name cd -Value z -Option AllScope
+Set-Alias -Name vi -Value nvim 
+Set-Alias -Name vim -Value nvim
+
+# STARSHIP
+#-------------------------
+Invoke-Expression (&starship init powershell)
+
+# ZOXIDE
+#-------------------------
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
+
+# Skip fastfetch for non-interactive shells
+if ([Environment]::GetCommandLineArgs().Contains("-NonInteractive")) {
+  return
+}
+fastfetch
