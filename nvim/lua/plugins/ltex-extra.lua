56524c1706a58e96aa370da0bb65a2ab60e11d51
hfrisk <hfrisk+githubpublic@protonmail.com>
2025-08-20 12:16:08 -0500

    Added ltex LSP

---
+++ nvim/lua/plugins/ltex-extra.lua
@@ -0,0 +1,20 @@
+return {
+    "barreiroleo/ltex_extra.nvim",
+    ft = { "markdown", "tex", "md", "asciidoc", "adoc" },
+    dependencies = { "neovim/nvim-lspconfig" },
+    -- yes, you can use the opts field, just I'm showing the setup explicitly
+    config = function()
+        require("ltex_extra").setup {
+            --your_ltex_extra_opts,
+            server_opts = {
+                --capabilities = your_capabilities,
+                on_attach = function(client, bufnr)
+                    -- your on_attach process
+                end,
+                settings = {
+                    --ltex = { your settings }
+                }
+            },
+        }
+    end
+}
