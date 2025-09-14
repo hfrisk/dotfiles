9c49ca472e5dda57cb22d745285dc83efd07c91b
hfrisk <hfrisk+githubpublic@protonmail.com>
2025-09-14 16:15:27 -0500

    Added git-files.yazi

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,4 +1,9 @@
-require("full-border"):setup({type = ui.Border.PLAIN})
+require("full-border"):setup({ type = ui.Border.PLAIN })
 require("starship"):setup({
     config_file = "~/.dots/starline.toml",
 })
+
+th.git = th.git or {}
+th.git.added_sign = "+"
+th.git.modified_sign = "M"
+require("git"):setup()
