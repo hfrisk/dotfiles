101efc72e3ccd365dd03a8a062325ddda215b77f
hfrisk <hfrisk+githubpublic@protonmail.com>
2025-09-14 15:54:29 -0500

    Added project.nvim spec

---
+++ nvim/lua/plugins/project.lua
@@ -0,0 +1,23 @@
+return {
+    "ahmedkhalf/project.nvim",
+    event = "VeryLazy",
+    opts = {
+        detection_methods = { "lsp", "pattern" },
+        manual_mode = false
+    },
+    config = function(_, opts)
+        require("project_nvim").setup(opts)
+        local history = require("project_nvim.utils.history")
+        history.delete_project = function(project)
+            for k, v in pairs(history.recent_projects) do
+                if v == project.value then
+                    history.recent_projects[k] = nil
+                    return
+                end
+            end
+        end
+        LazyVim.on_load("telescope.nvim", function()
+            require("telescope").load_extension("projects")
+        end)
+    end,
+}
